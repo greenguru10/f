80310fc1dc8b38c5e0aa022bf7a413b07fadd008
vedant <vedantskills1@gmail.com>
2025-08-09 21:41:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh', fontFamily: 'Arial' }}>
+      <h1>🚧 We'll be back soon!</h1>
+      <p>Our site is currently undergoing maintenance.<br /> Please check back later.</p>
+    </div>
+  );
+}
